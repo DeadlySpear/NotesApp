c197811d4fe079e8fe62095e553a90435f4f2c56
DeadlySpear <svofficial.sv@gmail.com>
2025-07-30 21:42:33 +0530

    Login Added

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Container } from 'react-bootstrap';
 import {Routes, Route, Navigate} from "react-router-dom"
 import NewNote from './NewNote.tsx';
+import Login from './login.tsx'
 
 export type Note = {
   id: string
@@ -22,7 +23,7 @@ function App() {
         <Route path="/" element={<h1>home</h1>}></Route>
         <Route path="/new" element={<NewNote/>}></Route>
         <Route path="*" element={<Navigate to="/"/>}></Route>
-        
+        <Route path="/login" element={<Login />}></Route>
         <Route path="/:id">
           <Route index element={<h1>show</h1>}></Route>
           <Route path="edit" element={<h1>edit</h1>}></Route>
